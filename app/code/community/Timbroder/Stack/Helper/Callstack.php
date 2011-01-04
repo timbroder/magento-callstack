0b22195dff5a57b022eabca35b1afaeb4b969d92
broderboy <none@none>
2011-01-04 16:09:39 -0500

    initial

---
+++ app/code/community/Timbroder/Stack/Helper/Callstack.php
@@ -0,0 +1,25 @@
+<?php
+
+class Timbroder_Stack_Helper_Callstack extends Mage_Core_Helper_Abstract
+{
+	private function get_callstack($delim="\n") {
+	  $dt = debug_backtrace();
+	  $cs = '';
+	  foreach ($dt as $t) {
+	    $cs .= $t['file'] . ' line ' . $t['line'] . ' calls ' . $t['function'] . "()" . $delim;
+	  }
+	
+	  return $cs;
+	}    
+	
+	public function toLog() {
+		Mage::log($this->get_callstack());
+	}
+	
+	function toFirePhp() {
+		$stack = $this->get_callstack();
+		foreach (explode("\n", $stack) as $line) {
+			Mage::helper('firephp')->send($line);
+		}
+	}
+}
